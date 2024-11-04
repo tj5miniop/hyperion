f4d50b5c2957dbd2118c18b4cdda820b04933ec1
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2024-11-04 17:27:30 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,8 +13,11 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
-
+# Install LATEST Cosmic
+rpm-ostree install curl 
+curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-41/ryanabx-cosmic-epoch-fedora-41.repo
+rpm-ostree install flatpak distrobox podman
+rpm-ostree install sddm cosmic-desktop
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
