ea4ed2d59a352eb3eb195bf0b365e1d79ac3a082
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2024-11-04 19:21:19 +0000

    Create postsetup.sh

---
+++ postsetup.sh
@@ -0,0 +1,5 @@
+flatpak install flathub org.mozilla.firefox
+flatpak install org.mozilla.Thunderbird
+flatpak install flathub io.github.dvlv.boxbuddyrs
+flatpak install flathub dev.vencord.Vesktop
+echo Done!
