b8293cf2d010be1ffeccc220de7d29975bdf6480
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2026-09-12 13:42:24 +0100

    add installation to fluent
    missed this in previous commit

--- build_files/build.sh
+++ build_files/build.sh
@@ -90,6 +90,9 @@ DIR_FLUENT=/tmp/themes/
 REPO_NAME=Fluent-kde
 mkdir -p $DIR_FLUENT && cd $DIR_FLUENT
 git clone https://github.com/vinceliuice/"$REPO_NAME" && cd $REPO_NAME
+bash ./install.sh
+
+cd /tmp
 
 
 # ----------------------------
