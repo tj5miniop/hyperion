749dbeebf26141f74191d1e81cc0a41c260d2eec
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2026-09-20 17:39:56 +0100

    chore: comment out uneeded commands

--- build_files/htpc.sh
+++ build_files/htpc.sh
@@ -1,7 +1,7 @@
 # Hyperion HTPC edition
 
-# Copy system files
-cp -avf "/ctx/system_files/htpc"/. /
+# Copy system files - NOT NEEDED AS OF NOW AS IT USES THE ASUS FILES
+#cp -avf "/ctx/system_files/htpc"/. /
 
 # Install certain HTPC-Related Packages
 dnf -y install \
@@ -15,7 +15,6 @@ dnf -y install \
 # Install nested gamescope session
 GIT_REPO=https://github.com/tj5miniop/gamescope-session-nested
 GSC_SES_DIR=/tmp/
-
 cd $GSC_SES_DIR
 git clone $GIT_REPO
 cd gamescope-session-nested && ./install.sh
