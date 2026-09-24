73d6136e8ce22536171717d2e1763cc43f77c4f4
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2026-09-24 11:24:39 +0100

    expand htpc tooling

--- build_files/htpc.sh
+++ build_files/htpc.sh
@@ -1,10 +1,31 @@
-# Hyperion HTPC edition
+#!/usr/bin/bash
+# Hyperion HTPC edition (couch-gaming focused)
+#
+# NOTE: package names are from memory and depend on which repos your base
+# image enables (Fedora, RPM Fusion, the Bazzite COPRs). Verify each with:
+#   dnf repoquery <name>
+set -euxo pipefail
 
 # Copy system files - NOT NEEDED AS OF NOW AS IT USES THE ASUS FILES
 #cp -avf "/ctx/system_files/htpc"/. /
 
-# Install certain HTPC-Related Packages
 dnf -y install \
     gamescope-session-steam \
-    inputplumber \
     gamescope-session \
+    inputplumber
+
+# Controllers and gaming utilities
+dnf -y install \
+    8bitdo-udev-rules \
+    wooting-udev-rules \
+    gamemode \
+    mangohud
+
+# Networking Tools
+dnf -y install \
+    cifs-utils \
+    nfs-utils \
+    openssh-server
+
+# Cleanup
+dnf clean all
