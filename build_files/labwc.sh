c8044cedb445005c28191abbcd1c25a943719ecd
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2026-09-25 23:54:47 +0100

    fix labwc images

--- build_files/labwc.sh
+++ build_files/labwc.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
+set -ouex pipefail
 
-dnf5 -y install ly NetworkManager xorg-x11-server-xwayland egl-wayland wayland-utils quickshell labwc alacritty git polkit polkit-gnome
+dnf5 -y copr enable errornointernet/quickshell
+dnf5 -y install \
+    ly NetworkManager xorg-x11-server-xwayland wayland-utils \
+    quickshell labwc alacritty git polkit polkit-gnome
+dnf5 -y copr disable errornointernet/quickshell
+
+# Enable SystemD Stuff
+systemctl enable NetworkManager
+systemctl enable ly@tty1
+systemctl set-default graphical.target
 
 # Install dotfiles - possibly install them to skel and implement a ujust dotfiles-update command to update the dots
-WORK_DIR_DOTS = /tmp/dots/
-mkdir -p $WORK_DIR_DOTS && cd $WORK_DIR_DOTS
-git clone https://github.com/tj5miniop/tj5-de/
-# Copy to Skel
-cd tj5-de && cp -r .config/ /etc/skel/
-
+WORK_DIR_DOTS=/tmp/dots/
+mkdir -p "$WORK_DIR_DOTS" && cd "$WORK_DIR_DOTS"
+git clone --depth 1 https://github.com/tj5miniop/tj5-de/
+# Copy to Skel, for any user created after first boot...
+cp -r tj5-de/.config/ /etc/skel/
+# ...and to root
+cp -r tj5-de/.config/ /root/
 # Return back to root
 cd /
+rm -rf "$WORK_DIR_DOTS"
 
 # Copy LABWC files to system
-# Copy all files to root directory
 cp -avf "/ctx/system_files/labwc"/. /
