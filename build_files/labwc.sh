2469a58b1c21c6ce17fdb21a1c3621955a94b3c7
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2026-09-26 00:16:16 +0100

    switch to polkit KDE

--- build_files/labwc.sh
+++ build_files/labwc.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 dnf5 -y copr enable errornointernet/quickshell
 dnf5 -y install \
     ly NetworkManager xorg-x11-server-xwayland wayland-utils \
-    quickshell labwc alacritty git polkit polkit-gnome
+    quickshell labwc alacritty git polkit polkit-kde
 dnf5 -y copr disable errornointernet/quickshell
 
 # Enable SystemD Stuff
